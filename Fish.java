9db4466fa0191bf1932e01a3084f08605a6a93f5
sohita789 <sohitaxworkz@gmail.com>
2024-07-31 16:46:07 +0530

    task

---
+++ Fish.java
@@ -0,0 +1,109 @@
+class Fish{
+String name;
+double weight;
+Fish(String name,double weight){//constructor
+System.out.println("creating the copy Fish using name,weight");
+System.out.println("Fish name:"+name);
+System.out.println("Fish weight:"+weight);
+}
+}
+
+class Lamp{
+String brand;
+int quantity;
+Lamp(String brand,int quantity){
+System.out.println("creating the copy Lamp using brand,quantity");
+System.out.println("Lamp brand:"+brand);
+System.out.println("Lamp quantity:"+quantity);
+}
+}
+
+class PrintingMachine{
+String brand;
+float weight;
+PrintingMachine(String brand,float weight){
+System.out.println("creating the copy PrintingMachine using brand,weight");
+System.out.println("PrintingMachine brand:"+brand);
+System.out.println("PrintingMachine weight:"+weight);
+}
+}
+
+class Lens{
+String type;
+int noOfLens;
+Lens(String type,int noOfLens){
+System.out.println("creating the  copy Lens using type,noOfLens");
+System.out.println("Lens type:"+type);
+System.out.println("Lens noOfLens:"+noOfLens);
+}
+}
+
+class Coil{
+String name;
+double price;
+Coil(String name,double price){
+System.out.println("creating the copy Coil using name,price");
+System.out.println("Coil name:"+name);
+System.out.println("Coil price:"+price);
+}
+}
+
+class Capacitor{
+String name;
+double price;
+int quantity;
+Capacitor(String name,double price,int quantity){
+System.out.println("creating the copy Capacitor using name,price,quantity");
+System.out.println("Capacitor name:"+name);
+System.out.println("Capacitor price:"+price);
+System.out.println("Capacitor quantity:"+quantity);
+}
+}
+
+class Gold{
+String shopname;
+double price;
+int carat;
+Gold(String shopname,double price,int carat){
+System.out.println("creating the copy Gold using shopname,price,carat");
+System.out.println("Gold shopname:"+shopname);
+System.out.println("Gold price:"+price);
+System.out.println("Gold carat:"+carat);
+}
+}
+
+class Board{
+String color;
+double width;
+double price;
+Board(String color,double width,double price){
+System.out.println("creating the copy Board using color,width,price");
+System.out.println("Board color:"+color);
+System.out.println("Board width:"+width);
+System.out.println("Board price:"+price);
+}
+}
+
+class Led{
+String name;
+double price;
+int quantity;
+Led(String name,double price,int quantity){
+System.out.println("creating the copy Led using name,price,quantity");
+System.out.println("Led name:"+name);
+System.out.println("Led price:"+price);
+System.out.println("Led quantity:"+quantity);
+}
+}
+
+class Simcard{
+String name;
+double price;
+int noOf;
+Simcard(String name,double price,int noOf){
+System.out.println("creating the copy Simcard using name,price,noOf");
+System.out.println("Simcard name:"+name);
+System.out.println("Simcard price:"+price);
+System.out.println("Simcard noOf:"+noOf);
+}
+}
---
+++ FishRunner.java
@@ -0,0 +1,518 @@
+class FishRunner{
+public static void main(String[]args)
+{
+System.out.println("starting the main in FishRunner");
+
+Fish fish=new Fish("Anjal",6);
+System.out.println("Fish name:"+fish.name);
+System.out.println("Fish weight:"+fish.weight);
+
+
+Fish fish1=new Fish("silver",5);
+System.out.println("Fish name:"+fish1.name);
+System.out.println("Fish weight:"+fish1.weight);
+
+Fish fish2=new Fish("goldfish",7);
+System.out.println("Fish name:"+fish2.name);
+System.out.println("Fish weight:"+fish2.weight);
+
+Fish fish3=new Fish("salmon",3);
+System.out.println("Fish name:"+fish3.name);
+System.out.println("Fish weight:"+fish3.weight);
+
+Fish fish4=new Fish("tuna",4);
+System.out.println("Fish name:"+fish4.name);
+System.out.println("Fish weight"+fish4.weight);
+
+Fish fish5=new Fish("cod",5);
+System.out.println("Fish name:"+fish5.name);
+System.out.println("Fish weight:"+fish5.weight);
+
+Fish fish6=new Fish("trout",8);
+System.out.println("Fish name:"+fish6.name);
+System.out.println("Fish weight"+fish6.weight);
+
+Fish fish7=new Fish("bass",9);
+System.out.println("Fish name:"+fish7.name);
+System.out.println("Fish weight:"+fish7.weight);
+
+Fish fish8=new Fish("haddock",5);
+System.out.println("Fish name:"+fish8.name);
+System.out.println("Fish weight:"+fish8.weight);
+
+Fish fish9=new Fish("catfish",7);
+System.out.println("Fish name:"+fish9.name);
+System.out.println("Fish weight:"+fish9.weight);
+
+Fish fish10=new Fish("snapper",7);
+System.out.println("Fish name:"+fish10.name);
+System.out.println("Fish weight:"+fish10.weight);
+
+
+
+Lamp lamp=new Lamp("philips",6);
+System.out.println("Lamp brand:"+lamp.brand);
+System.out.println("Lamp quantity:"+lamp.quantity);
+
+Lamp lamp1=new Lamp("osram",1);
+System.out.println("Lamp brand:"+lamp1.brand);
+System.out.println("Lamp quantity:"+lamp1.quantity);
+
+Lamp lamp2=new Lamp("cree",2);
+System.out.println("Lamp brand:"+lamp2.brand);
+System.out.println("Lamp quantity:"+lamp2.quantity);
+
+Lamp lamp3=new Lamp("lifx",3);
+System.out.println("Lamp brand:"+lamp3.brand);
+System.out.println("Lamp quantity:"+lamp3.quantity);
+
+Lamp lamp4=new Lamp("tcp",4);
+System.out.println("Lamp brand:"+lamp4.brand);
+System.out.println("Lamp quantity:"+lamp4.quantity);
+
+Lamp lamp5=new Lamp("brightech",5);
+System.out.println("Lamp brand:"+lamp5.brand);
+System.out.println("Lamp quantity:"+lamp5.quantity);
+
+Lamp lamp6=new Lamp("halco",6);
+System.out.println("Lamp brand:"+lamp6.brand);
+System.out.println("Lamp quantity:"+lamp6.quantity);
+
+Lamp lamp7=new Lamp("sylvania",7);
+System.out.println("Lamp brand:"+lamp7.brand);
+System.out.println("Lamp quantity:"+lamp7.quantity);
+
+Lamp lamp8=new Lamp("bulbrite",8);
+System.out.println("Lamp brand:"+lamp8.brand);
+System.out.println("Lamp quantity:"+lamp8.quantity);
+
+Lamp lamp9=new Lamp("GE",9);
+System.out.println("Lamp brand:"+lamp9.brand);
+System.out.println("Lamp quantity:"+lamp9.quantity);
+
+Lamp lamp10=new Lamp("feitElectric",6);
+System.out.println("Lamp brand:"+lamp10.brand);
+System.out.println("Lamp quantity:"+lamp10.quantity);
+
+
+PrintingMachine print=new PrintingMachine("philips",6f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print1=new PrintingMachine("samsung",16f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print2=new PrintingMachine("sony",5f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print3=new PrintingMachine("konica",4f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print4=new PrintingMachine("dell",7f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print5=new PrintingMachine("lemark",8f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print6=new PrintingMachine("xerox",9f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print7=new PrintingMachine("epson",20f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print8=new PrintingMachine("hp",10f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print9=new PrintingMachine("brother",11f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+PrintingMachine print10=new PrintingMachine("canon",12f);
+System.out.println("PrintingMachine brand:"+print.brand);
+System.out.println("PrintingMachine weight:"+print.weight);
+
+
+Lens spects=new Lens("concave",1);
+System.out.println("Lens type:"+spects.type);
+System.out.println("Lens noOfLens:"+spects.noOfLens);
+
+Lens spects1=new Lens("convex",2);
+System.out.println("Lens type:"+spects1.type);
+System.out.println("Lens noOfLens:"+spects1.noOfLens);
+
+Lens spects2=new Lens("biconcave",3);
+System.out.println("Lens type:"+spects2.type);
+System.out.println("Lens noOfLens:"+spects2.noOfLens);
+
+Lens spects3=new Lens("biconvex",4);
+System.out.println("Lens type:"+spects3.type);
+System.out.println("Lens noOfLens:"+spects3.noOfLens);
+
+Lens spects4=new Lens("planoconvex",5);
+System.out.println("Lens type:"+spects4.type);
+System.out.println("Lens noOfLens:"+spects4.noOfLens);
+
+Lens spects5=new Lens("planoconcave",6);
+System.out.println("Lens type:"+spects5.type);
+System.out.println("Lens noOfLens:"+spects5.noOfLens);
+
+Lens spects6=new Lens("fresnel",7);
+System.out.println("Lens type:"+spects6.type);
+System.out.println("Lens noOfLens:"+spects6.noOfLens);
+
+Lens spects7=new Lens("aspheric",8);
+System.out.println("Lens type:"+spects7.type);
+System.out.println("Lens noOfLens:"+spects7.noOfLens);
+
+Lens spects8=new Lens("cylindical",9);
+System.out.println("Lens type:"+spects8.type);
+System.out.println("Lens noOfLens:"+spects8.noOfLens);
+
+Lens spects9=new Lens("concave-convex",10);
+System.out.println("Lens type:"+spects9.type);
+System.out.println("Lens noOfLens:"+spects9.noOfLens);
+
+Lens spects10=new Lens("concaves",11);
+System.out.println("Lens type:"+spects10.type);
+System.out.println("Lens noOfLens:"+spects10.noOfLens);
+
+
+Coil coil=new Coil("mortein",5);
+System.out.println("Coil name:"+coil.name);
+System.out.println("Coil price:"+coil.price);
+
+Coil coil1=new Coil("solenoid",60);
+System.out.println("Coil name:"+coil1.name);
+System.out.println("Coil price:"+coil1.price);
+
+Coil coil2=new Coil("toroidal",70);
+System.out.println("Coil name:"+coil2.name);
+System.out.println("Coil price:"+coil2.price);
+
+Coil coil3=new Coil("helical",80);
+System.out.println("Coil name:"+coil3.name);
+System.out.println("Coil price:"+coil3.price);
+
+Coil coil4=new Coil("aircore",90);
+System.out.println("Coil name:"+coil4.name);
+System.out.println("Coil price:"+coil4.price);
+
+Coil coil5=new Coil("ferrite",100);
+System.out.println("Coil name:"+coil5.name);
+System.out.println("Coil price:"+coil5.price);
+
+Coil coil6=new Coil("rogowski",110);
+System.out.println("Coil name:"+coil6.name);
+System.out.println("Coil price:"+coil6.price);
+
+Coil coil7=new Coil("choke",120);
+System.out.println("Coil name:"+coil7.name);
+System.out.println("Coil price:"+coil7.price);
+
+Coil coil8=new Coil("variable",150);
+System.out.println("Coil name:"+coil8.name);
+System.out.println("Coil price:"+coil8.price);
+
+Coil coil9=new Coil("transform",250);
+System.out.println("Coil name:"+coil9.name);
+System.out.println("Coil price:"+coil9.price);
+
+Coil coil10=new Coil("ironcore",130);
+System.out.println("Coil name:"+coil10.name);
+System.out.println("Coil price:"+coil10.price);
+
+Capacitor cap=new Capacitor("through hole",1200,1);
+System.out.println("Capacitor name:"+cap.name);
+System.out.println("Capacitor price:"+cap.price);
+System.out.println("Capacitor quantity:"+cap.quantity);
+
+Capacitor cap1=new Capacitor("surface mount",250,2);
+System.out.println("Capacitor name:"+cap1.name);
+System.out.println("Capacitor price:"+cap1.price);
+System.out.println("Capacitor quantity:"+cap.quantity);
+
+Capacitor cap2=new Capacitor("chip",2500,10);
+System.out.println("Capacitor name:"+cap2.name);
+System.out.println("Capacitor price:"+cap2.price);
+System.out.println("Capacitor quantity:"+cap2.quantity);
+
+Capacitor cap3=new Capacitor("electrolytic",1225,15);
+System.out.println("Capacitor name:"+cap3.name);
+System.out.println("Capacitor price:"+cap3.price);
+System.out.println("Capacitor quantity:"+cap3.quantity);
+
+Capacitor cap4=new Capacitor("tantalum",257,12);
+System.out.println("Capacitor name:"+cap4.name);
+System.out.println("Capacitor price:"+cap4.price);
+System.out.println("Capacitor quantity:"+cap4.quantity);
+
+Capacitor cap5=new Capacitor("film capacitor",1025,17);
+System.out.println("Capacitor name:"+cap5.name);
+System.out.println("Capacitor price:"+cap5.price);
+System.out.println("Capacitor quantity:"+cap5.quantity);
+
+Capacitor cap6=new Capacitor("ceramic",350,19);
+System.out.println("Capacitor name:"+cap6.name);
+System.out.println("Capacitor price:"+cap6.price);
+System.out.println("Capacitor quantity:"+cap6.quantity);
+
+Capacitor cap7=new Capacitor("aliminium",300,10);
+System.out.println("Capacitor name:"+cap7.name);
+System.out.println("Capacitor price:"+cap7.price);
+System.out.println("Capacitor quantity:"+cap7.quantity);
+
+Capacitor cap8=new Capacitor("mica",2550,9);
+System.out.println("Capacitor name:"+cap8.name);
+System.out.println("Capacitor price:"+cap8.price);
+System.out.println("Capacitor quantity:"+cap8.quantity);
+
+Capacitor cap9=new Capacitor("supercapacitor",3500,8);
+System.out.println("Capacitor name:"+cap9.name);
+System.out.println("Capacitor price:"+cap9.price);
+System.out.println("Capacitor quantity:"+cap9.quantity);
+
+Capacitor cap10=new Capacitor("mount",2300,1000);
+System.out.println("Capacitor name:"+cap10.name);
+System.out.println("Capacitor price:"+cap10.price);
+System.out.println("Capacitor quantity:"+cap10.quantity);
+
+
+
+Gold gold=new Gold("malabar",6197,22);
+System.out.println("Gold shopname:"+gold.shopname);
+System.out.println("Gold price:"+gold.price);
+System.out.println("Gold carat:"+gold.carat);
+
+Gold gold1=new Gold("malabar",6197,24);
+System.out.println("Gold shopname:"+gold1.shopname);
+System.out.println("Gold price:"+gold1.price);
+System.out.println("Gold carat:"+gold1.carat);
+
+Gold gold2=new Gold("mahalaxmi",6196,14);
+System.out.println("Gold shopname:"+gold2.shopname);
+System.out.println("Gold price:"+gold2.price);
+System.out.println("Gold carat:"+gold2.carat);
+
+Gold gold3=new Gold("khajana",6195,12);
+System.out.println("Gold shopname:"+gold3.shopname);
+System.out.println("Gold price:"+gold3.price);
+System.out.println("Gold carat:"+gold3.carat);
+
+Gold gold4=new Gold("tanishq",6194,10);
+System.out.println("Gold shopname:"+gold4.shopname);
+System.out.println("Gold price:"+gold4.price);
+System.out.println("Gold carat:"+gold4.carat);
+
+Gold gold5=new Gold("goldhub",6198,18);
+System.out.println("Gold shopname:"+gold5.shopname);
+System.out.println("Gold price:"+gold5.price);
+System.out.println("Gold carat:"+gold5.carat);
+
+Gold gold6=new Gold("kalyan",6190,12);
+System.out.println("Gold shopname:"+gold6.shopname);
+System.out.println("Gold price:"+gold6.price);
+System.out.println("Gold carat:"+gold6.carat);
+
+Gold gold7=new Gold("senco",6191,22);
+System.out.println("Gold shopname:"+gold7.shopname);
+System.out.println("Gold price:"+gold7.price);
+System.out.println("Gold carat:"+gold7.carat);
+
+Gold gold8=new Gold("chowdaih",6197,24);
+System.out.println("Gold shopname:"+gold8.shopname);
+System.out.println("Gold price:"+gold8.price);
+System.out.println("Gold carat:"+gold8.carat);
+
+Gold gold9=new Gold("joyalukkas",6197,10);
+System.out.println("Gold shopname:"+gold9.shopname);
+System.out.println("Gold price:"+gold9.price);
+System.out.println("Gold carat:"+gold9.carat);
+
+Gold gold10=new Gold("bhima",6197,26);
+System.out.println("Gold shopname:"+gold10.shopname);
+System.out.println("Gold price:"+gold10.price);
+System.out.println("Gold carat:"+gold10.carat);
+
+
+
+Board board=new Board("black",25,1200);
+System.out.println("Board color:"+board.color);
+System.out.println("Board width:"+board.width);
+System.out.println("Board price:"+board.price);
+
+Board board1=new Board("white",26,1300);
+System.out.println("Board color:"+board1.color);
+System.out.println("Board width:"+board1.width);
+System.out.println("Board price:"+board1.price);
+
+Board board2=new Board("green",24,1220);
+System.out.println("Board color:"+board2.color);
+System.out.println("Board width:"+board2.width);
+System.out.println("Board price:"+board2.price);
+
+Board board3=new Board("blue",26,1400);
+System.out.println("Board color:"+board3.color);
+System.out.println("Board width:"+board3.width);
+System.out.println("Board price:"+board3.price);
+
+Board board4=new Board("smartBoard",30,10200);
+System.out.println("Board color:"+board4.color);
+System.out.println("Board width:"+board4.width);
+System.out.println("Board price:"+board4.price);
+
+Board board5=new Board("pink",20,1250);
+System.out.println("Board color:"+board5.color);
+System.out.println("Board width:"+board5.width);
+System.out.println("Board price:"+board5.price);
+
+Board board6=new Board("red",34,1500);
+System.out.println("Board color:"+board6.color);
+System.out.println("Board width:"+board6.width);
+System.out.println("Board price:"+board6.price);
+
+Board board7=new Board("orange",27,1600);
+System.out.println("Board color:"+board7.color);
+System.out.println("Board width:"+board7.width);
+System.out.println("Board price:"+board7.price);
+
+Board board8=new Board("purple",35,2000);
+System.out.println("Board color:"+board8.color);
+System.out.println("Board width:"+board8.width);
+System.out.println("Board price:"+board8.price);
+
+Board board9=new Board("brown",40,1270);
+System.out.println("Board color:"+board9.color);
+System.out.println("Board width:"+board9.width);
+System.out.println("Board price:"+board9.price);
+
+Board board10=new Board("gray",25,1350);
+System.out.println("Board color:"+board10.color);
+System.out.println("Board width:"+board10.width);
+System.out.println("Board price:"+board10.price);
+
+
+Led light=new Led("standard LED",300,1);
+System.out.println("Led name:"+light.name);
+System.out.println("Led price:"+light.price);
+System.out.println("Led quantity:"+light.quantity);
+
+Led light1=new Led("high_power",310,2);
+System.out.println("Led name:"+light1.name);
+System.out.println("Led price:"+light1.price);
+System.out.println("Led quantity:"+light1.quantity);
+
+Led light2=new Led("RGB",340,3);
+System.out.println("Led name:"+light2.name);
+System.out.println("Led price:"+light2.price);
+System.out.println("Led quantity:"+light2.quantity);
+
+Led light3=new Led("SMD",349,4);
+System.out.println("Led name:"+light3.name);
+System.out.println("Led price:"+light3.price);
+System.out.println("Led quantity:"+light3.quantity);
+
+Led light4=new Led("COB",250,15);
+System.out.println("Led name:"+light4.name);
+System.out.println("Led price:"+light4.price);
+System.out.println("Led quantity:"+light4.quantity);
+
+Led light5=new Led("UV",290,6);
+System.out.println("Led name:"+light5.name);
+System.out.println("Led price:"+light5.price);
+System.out.println("Led quantity:"+light5.quantity);
+
+Led light6=new Led("infrared",400,9);
+System.out.println("Led name:"+light6.name);
+System.out.println("Led price:"+light6.price);
+System.out.println("Led quantity:"+light6.quantity);
+
+Led light7=new Led("addressable",400,4);
+System.out.println("Led name:"+light7.name);
+System.out.println("Led price:"+light7.price);
+System.out.println("Led quantity:"+light7.quantity);
+
+Led light8=new Led("smartLeds",390,8);
+System.out.println("Led name:"+light8.name);
+System.out.println("Led price:"+light8.price);
+System.out.println("Led quantity:"+light8.quantity);
+
+Led light9=new Led("organiclight",360,10);
+System.out.println("Led name:"+light9.name);
+System.out.println("Led price:"+light9.price);
+System.out.println("Led quantity:"+light9.quantity);
+
+Led light10=new Led("emitting",600,20);
+System.out.println("Led name:"+light10.name);
+System.out.println("Led price:"+light10.price);
+System.out.println("Led quantity:"+light10.quantity);
+
+
+Simcard sim=new Simcard("airtel",450,2);
+System.out.println("Simcard name:"+sim.name);
+System.out.println("Simcard price:"+sim.price);
+System.out.println("Simcard noOf:"+sim.noOf);
+
+Simcard sim1=new Simcard("jio",450,2);
+System.out.println("Simcard name:"+sim1.name);
+System.out.println("Simcard price:"+sim1.price);
+System.out.println("Simcard noOf:"+sim1.noOf);
+
+
+Simcard sim2=new Simcard("vodafone",450,2);
+System.out.println("Simcard name:"+sim2.name);
+System.out.println("Simcard price:"+sim2.price);
+System.out.println("Simcard noOf:"+sim2.noOf);
+
+Simcard sim3=new Simcard("BSNL",450,2);
+System.out.println("Simcard name:"+sim3.name);
+System.out.println("Simcard price:"+sim3.price);
+System.out.println("Simcard noOf:"+sim3.noOf);
+
+Simcard sim4=new Simcard("docomo",450,2);
+System.out.println("Simcard name:"+sim4.name);
+System.out.println("Simcard price:"+sim4.price);
+System.out.println("Simcard noOf:"+sim4.noOf);
+
+Simcard sim5=new Simcard("telstra",450,2);
+System.out.println("Simcard name:"+sim5.name);
+System.out.println("Simcard price:"+sim5.price);
+System.out.println("Simcard noOf:"+sim5.noOf);
+
+Simcard sim6=new Simcard("optus",450,2);
+System.out.println("Simcard name:"+sim6.name);
+System.out.println("Simcard price:"+sim6.price);
+System.out.println("Simcard noOf:"+sim6.noOf);
+
+Simcard sim7=new Simcard("verizon",450,2);
+System.out.println("Simcard name:"+sim7.name);
+System.out.println("Simcard price:"+sim7.price);
+System.out.println("Simcard noOf:"+sim7.noOf);
+
+Simcard sim8=new Simcard("china",450,2);
+System.out.println("Simcard name:"+sim8.name);
+System.out.println("Simcard price:"+sim8.price);
+System.out.println("Simcard noOf:"+sim8.noOf);
+
+Simcard sim9=new Simcard("T-mobile",450,2);
+System.out.println("Simcard name:"+sim9.name);
+System.out.println("Simcard price:"+sim9.price);
+System.out.println("Simcard noOf:"+sim9.noOf);
+
+Simcard sim10=new Simcard("AT&T",450,2);
+System.out.println("Simcard name:"+sim10.name);
+System.out.println("Simcard price:"+sim10.price);
+System.out.println("Simcard noOf:"+sim10.noOf);
+
+System.out.println("end main in FishRunner");
+}
+}
+
+
