8a078c080b0c0948423f7d52672735f9f9e0f676
sohita789 <sohitaxworkz@gmail.com>
2024-08-02 12:47:18 +0530

    task

---
+++ ChargerRunner.java
@@ -0,0 +1,83 @@
+class ChargerRunner
+{
+public static void main(String[]args)
+{
+Charger charger=new Charger();
+charger.price=200;
+charger.quantity=10;
+System.out.println("charger name:"+charger.name);
+System.out.println("charger price:"+charger.price);
+System.out.println("charger quantity:"+charger.quantity);
+		
+Claw claw=new Claw();
+claw.type="bite";
+claw.depth=10;
+System.out.println("claw  name:"+claw.name);
+System.out.println("claw type:"+claw.type);
+System.out.println("claw depth:"+claw.depth);
+		
+Crow crow=new Crow();	   
+crow.weight=10;
+crow.quantity1=20;
+System.out.println("crow name:"+crow.type);
+System.out.println("crow weight:"+crow.weight);
+System.out.println("crow quantity:"+crow.quantity1);
+	
+		
+Clock clock=new Clock();
+clock.quantity2=2;
+clock.price=1200;
+System.out.println("clock brand:"+clock.brand);
+System.out.println("clock quantity2:"+clock.quantity2);
+System.out.println("clock price:"+clock.price);
+		
+	    
+Lolipop lolipop=new Lolipop();
+lolipop.company="alpenlibe";
+lolipop.quantity3=100;
+System.out.println("lolipop company:"+lolipop.company);
+System.out.println("lolipop flavour:"+lolipop.flavour);
+System.out.println("lolipop quantity3:"+lolipop.quantity3);
+	
+Jean jean=new Jean();
+jean.brand="max";
+jean.cost=1000;
+System.out.println("jean brand:"+jean.brand);
+System.out.println("jean cost:"+jean.cost);
+System.out.println("jean quantity4:"+jean.quantity4);
+		
+Keyboard keyboard=new Keyboard();
+keyboard.name="dell";
+keyboard.keys=100;
+System.out.println("keyboard name:"+keyboard.name);
+System.out.println("keyboard price:"+keyboard.price);
+System.out.println("keyboard keys:"+keyboard.keys);
+		
+Mountain mountain=new Mountain();
+mountain.place="udaygiri";
+mountain.height=5000f;
+System.out.println("mountain name:"+mountain.name);
+System.out.println("mountain place:"+mountain.place);
+System.out.println("mountain height:"+mountain.height);
+		
+Stadium stadium=new Stadium();
+stadium.name="chinnaswamy";
+stadium.area=200;
+System.out.println("stadium name:"+stadium.name);
+System.out.println("stadium location:"+stadium.location);
+System.out.println("stadium area:"+stadium.area);
+		
+Spray spray=new Spray();
+spray.quantity5=20;
+spray.price=109;
+System.out.println("spray quality:"+spray.quality);
+System.out.println("spray quantity:"+spray.quantity5);
+System.out.println("spray price:"+spray.price);
+	
+}
+}
+		
+		
+		
+		
+		
